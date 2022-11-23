11de29ee722a9638f6dea1738b12efb6f7b4e365
AlexisAparicio <90709059+AlexisAparicio@users.noreply.github.com>
2022-11-23 11:27:32 -0500

    Delete demo directory

--- demo/src/main/java/com/hellowworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.hellowworld;
-
-import static spark.Spark.*;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
-} 
