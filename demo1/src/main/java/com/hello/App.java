9f6efcdd761ca92dd580b213478f45f7291286c6
Bolivar Aparicio <alexiiz360@gmail.com>
2022-11-23 16:26:59 +0000

    update

--- demo1/src/main/java/com/hello/App.java
+++ demo1/src/main/java/com/hello/App.java
@@ -8,7 +8,7 @@ import static spark.Spark.*;
 public class App 
 {
         public static void main(String[] args) {
-            get("/wrx", (req, res) -> "Hello World test");
+            get("/wrx", (req, res) -> "Hello World test try");
             
         }
     }
